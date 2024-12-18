da0fd3b7aa13cd8b511008934293c7c8bb0cbf2f
orehren <o.rehren@gmail.com>
2024-12-18 11:54:26 +0100

    Initialize Plugin

--- actions/WriteText/mappings.py
+++ actions/WriteText/mappings.py
@@ -20,6 +20,7 @@ class KeyMapper:
     def _setup_xkb(self):
         try:
             self.xkb_context = xkb.Context()
+            log.debug(f"Attributes of context: {dir(self.xkb_context)}")
             names = self.xkb_context.keymap_new_from_names("evdev", None, "us", None, None)
             if names is None:
               log.error(f"Failed to setup xkbcommon: keymap_new_from_names returned NULL")
