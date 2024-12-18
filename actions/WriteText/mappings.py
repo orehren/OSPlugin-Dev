e81d4d8348e864c0554b9e1eff934697947b94a1
orehren <o.rehren@gmail.com>
2024-12-18 08:41:41 +0100

    Initialize Plugin

--- actions/WriteText/mappings.py
+++ actions/WriteText/mappings.py
@@ -29,7 +29,7 @@ class KeyMapper:
             self.xkb_keymap = None
             self.xkb_state = None
             
-    def map_char(self, char: str) -> List[int]:
+   def map_char(self, char: str) -> List[int]:
         if not self.xkb_state or not self.xkb_keymap:
              log.error("xkbcommon is not set up correctly.")
              return []
@@ -38,12 +38,23 @@ class KeyMapper:
         utf32_char = ord(char)
             
         found_keycodes = []
-        for keycode in self.xkb_state:
-          if self.xkb_state.key_get_utf32(keycode) == utf32_char:
-            found_keycodes.append(keycode)
-        
+        for keycode in self.xkb_keymap:
+            self.xkb_state.key_update_mask(keycode, 0,0,0,0)
+            symbols = self.xkb_state.key_get_syms(keycode)
+            if not symbols:
+                 continue
+                
+            for symbol in symbols:
+                if xkb.keysym_to_utf32(symbol) == utf32_char:
+                  found_keycodes.append(keycode)
+                  break
+            
         if not found_keycodes:
-             log.warning(f"No keycode found for character: {char} (UTF-32: {utf32_char})")
+            log.warning(f"No keycode found for character: {char} (UTF-32: {utf32_char})")
+        
+        keycodes.extend(found_keycodes)
+
+        return keycodes
         
         keycodes.extend(found_keycodes)
         return keycodes
